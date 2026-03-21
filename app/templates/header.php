8ff11c9b84b6b42f1d1e83548ca4c0a7cb2a1020
Cal Geek <javascript.uiux@gmail.com>
2026-03-21 11:11:46 -0700

    Create header.php

---
+++ app/templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responses Starter App</title>
+    <link rel="icon" href="/images/openai_logo.svg">
+
+    <!-- <link rel="stylesheet" href="/css/styles.css"> -->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+
+<body class="antialiased bg-gray-200 text-stone-900">
+    <div class="flex h-screen w-full flex-col">
+        <main>
